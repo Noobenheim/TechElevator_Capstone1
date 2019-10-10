e58d4fdb8ca2271b3527506b4f487f90464d5e67
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-10 14:39:59 -0400

    Added userinput class

---
+++ module-1/19_Capstone/java/src/main/java/com/techelevator/UserInput.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class UserInput {
+	Scanner scanner = new Scanner(System.in);
+	
+	public int getInt() {
+		try {
+			return Integer.parseInt(scanner.nextLine());
+		} catch( NumberFormatException e ) {
+			return 0;
+		}
+	}
+	public double getDouble() {
+		try {
+			return Double.parseDouble(scanner.nextLine());
+		} catch( NumberFormatException e ) {
+			return 0.0;
+		}
+	}
+	public String getString() {
+		return scanner.nextLine();
+	}
+}
